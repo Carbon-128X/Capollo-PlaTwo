2bb3f63d3bf97e3727fce7390589ffae067df5c5
AmirMahdi <carbon128x@gmail.com>
2026-07-26 17:57:38 +0330

    Calculating Morris board positions

--- Platwo/morrisboardwidget.cpp
+++ Platwo/morrisboardwidget.cpp
@@ -8,12 +8,23 @@ MorrisBoardWidget::MorrisBoardWidget(QWidget *parent) : QWidget(parent){
 
 void MorrisBoardWidget::setGame(NineMensMorris *g) {
     game = g;
+    calculatePositions();
     update();
 }
 
 void MorrisBoardWidget::paintEvent(QPaintEvent *) {
+    if(!game)
+        return;
+
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
+    painter.fillRect(rect(), QColor(25,25,25));
+
+    painter.setBrush(Qt::white);
+    painter.setPen(Qt::NoPen);
+
+    for(const QPoint &p : positions)
+        painter.drawEllipse(p,6,6);
 }
 
 void MorrisBoardWidget::resizeEvent(QResizeEvent *event) {
@@ -25,4 +36,51 @@ void MorrisBoardWidget::mousePressEvent(QMouseEvent *event) {
     QWidget::mousePressEvent(event);
 }
 
-void MorrisBoardWidget::calculatePositions(){}
+void MorrisBoardWidget::calculatePositions(){
+    positions.clear();
+
+    int w = width();
+    int h = height();
+    int s = qMin(w, h);
+    int margin = s / 10;
+    int outer = s - 2 * margin;
+    int middle = outer * 2 / 3;
+    int inner = outer / 3;
+    QPoint center(w / 2, h / 2);
+    int o = outer / 2;
+    int m = middle / 2;
+    int i = inner / 2;
+
+    // outer square in board
+
+    positions << QPoint(center.x()-o, center.y()-o);
+    positions << QPoint(center.x(),   center.y()-o);
+    positions << QPoint(center.x()+o, center.y()-o);
+    positions << QPoint(center.x()+o, center.y());
+    positions << QPoint(center.x()+o, center.y()+o);
+    positions << QPoint(center.x(),   center.y()+o);
+    positions << QPoint(center.x()-o, center.y()+o);
+    positions << QPoint(center.x()-o, center.y());
+
+    // middle square in board
+
+    positions << QPoint(center.x()-m, center.y()-m);
+    positions << QPoint(center.x(),   center.y()-m);
+    positions << QPoint(center.x()+m, center.y()-m);
+    positions << QPoint(center.x()+m, center.y());
+    positions << QPoint(center.x()+m, center.y()+m);
+    positions << QPoint(center.x(),   center.y()+m);
+    positions << QPoint(center.x()-m, center.y()+m);
+    positions << QPoint(center.x()-m, center.y());
+
+    // inner square in board
+
+    positions << QPoint(center.x()-i, center.y()-i);
+    positions << QPoint(center.x(),   center.y()-i);
+    positions << QPoint(center.x()+i, center.y()-i);
+    positions << QPoint(center.x()+i, center.y());
+    positions << QPoint(center.x()+i, center.y()+i);
+    positions << QPoint(center.x(),   center.y()+i);
+    positions << QPoint(center.x()-i, center.y()+i);
+    positions << QPoint(center.x()-i, center.y());
+}
